14dd58a340ddffe2f3c26f3c509a6b7d38e8b42b
Durgesh Pushpakar <durgeshpushpakar001@gmail.com>
2021-12-29 00:54:46 +0530

    #44 completed

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,9 @@ const express = require('express')
 connectToMongo();
 const app = express()
 const port = 3000
-
+// available routes
+app.use('/api/auth', require('./routes/auth.js'));
+app.use('/api/notes', require('./routes/notes.js'));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
