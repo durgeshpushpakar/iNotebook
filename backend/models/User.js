7f6bdd46598abe1a1d93f85875b5b0481b76066c
Durgesh Pushpakar <durgeshpushpakar001@gmail.com>
2021-12-29 17:00:29 +0530

    #45 completed

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 const UserSchema = new Schema({
     name:{
         type:String,
@@ -9,7 +10,7 @@ const UserSchema = new Schema({
         required:true,
         unique:true
     },
-    name:{
+    password:{
         type:String,
         required:true
     },
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router=express.Router();
+const User=require('../models/User');
 
-router.get('/', (req, res)=>{
-    obj={
-        a:"thios",
-        number:34
-    }
-    res.json(obj);
+// create a user using :post request
+// when 
+router.post('/', (req, res)=>{
+    console.log(req.body);
+    const user=User(req.body);
+    user.save();
+    res.send("auth route");
 })
 module.exports=router;
