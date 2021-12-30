ccf13de8813422da042a0000004893133f55d7a4
Durgesh Pushpakar <durgeshpushpakar001@gmail.com>
2021-12-30 23:52:23 +0530

    #49 completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router=express.Router();
 const User=require('../models/User');
 const { body, validationResult } = require('express-validator');
+let bcrypt = require('bcryptjs');
+let jwt = require('jsonwebtoken');
+const JWT_SECRET="durgeshisagoodbo$y";
 
 // create a user using :post /api/auth/createuser request no login required
 router.post('/createuser',
@@ -21,15 +24,25 @@ router.post('/createuser',
             if(user){
                 return res.status(400).json({error:"sorry this email is already registered!"});
             }
+            // hashing the password using bcrypt
+            const salt=await bcrypt.genSalt(10);
+            let secPass=await bcrypt.hash(req.body.password, salt);
             user =await  User.create({
                 name:req.body.name,
-                password:req.body.password,
+                password:secPass,
                 email:req.body.email
             })
-            res.json({"Nice":"nice"})
-            console.log(user)
+            const data={
+                user:{
+                    id:user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SECRET);
+            console.log(authToken);
+            // console.log(user)
+            res.json({authToken});
         }catch(err){
-            console.log(error.message);
+            console.log(err.message);
         }
     }
 )
