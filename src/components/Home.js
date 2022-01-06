a47a41e6f833a5bf1d300fbbba4a7bd61dac1040
Durgesh Pushpakar <durgeshpushpakar001@gmail.com>
2022-01-06 13:57:58 +0530

    #61 completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
-import React from "react";
-
-export default function home() {
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
+export default function Home() {
+  const context = useContext(noteContext);
+  const { notes, setNotes } = context;
   return (
     <div className="container my-3">
       <h2>Add a note</h2>
@@ -9,16 +11,28 @@ export default function home() {
           <label htmlFor="exampleInputEmail1" className="form-label">
             Title
           </label>
-          <input type="email" className="form-control" id="exampleInputEmail1" />
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="exampleInputPassword1" className="form-label">
             Description
           </label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
         </div>
         <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
           <label className="form-check-label" htmlFor="exampleCheck1">
             Check me out
           </label>
@@ -28,6 +42,9 @@ export default function home() {
         </button>
       </form>
       <h2 className="my-3">Your Notes</h2>
+	  {notes.map((e)=>{
+		  return e.title
+	  })}
     </div>
   );
 }
