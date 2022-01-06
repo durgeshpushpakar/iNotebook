173b08ec9041e3db07c24bffe8cbc5c13967c500
Durgesh Pushpakar <durgeshpushpakar001@gmail.com>
2022-01-06 14:00:24 +0530

    Revert "#60 completed"
    This reverts commit 62be38c79b0eda0ff743b507e56b7c096d4cb3f9.

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,7 +1,30 @@
 import NoteContext from "./noteContext";
+import { useState } from "react";
 const NoteState = (props) => {
+    const notesInitial=[
+        {
+          "_id": "61d48906d856345900f40b8e",
+          "user": "61ce01d69bc828b8269548cb",
+          "title": "new Note",
+          "description": "Work hard everyday",
+          "tag": "personal",
+          "date": "2022-01-04T17:51:02.453Z",
+          "__v": 0
+        },
+        {
+          "_id": "61d48906d856345900f40b90",
+          "user": "61ce01d69bc828b8269548cb",
+          "title": "new Note",
+          "description": "Work hard everyday",
+          "tag": "personal",
+          "date": "2022-01-04T17:51:02.586Z",
+          "__v": 0
+        }
+      ]
+
+    const [notes, setNotes] = useState(notesInitial)
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
